4cb715c7d39e61adaf7114d38d97361ec743089d
refaat1297 <a.refaat1297@gmail.com>
2023-03-19 07:57:15 +0200

    Feat 🚀: add ProfileInfo component

--- pages/[username]/index.vue
+++ pages/[username]/index.vue
@@ -5,37 +5,90 @@ const users = [
     name: "Ahmed Refaat",
     username: 'refaat1297',
     avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd1',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd2',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd3',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd4',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd5',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd6',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd7',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd8',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd9',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+  },
+  {
+    id: 'dd10',
+    name: "Ahmed Refaat",
+    username: 'refaat1297',
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
   }
 ]
 </script>
 
 <template>
-  <div class="profile-page min-h-screen bg-am-100 text-white">
-    <div class="max-w-screen-lg mx-auto">
+  <div class="profile-page min-h-screen bg-am-100 text-white py-4">
+    <div class="max-w-screen-lg px-6 md:px-12 xl:px-0 mx-auto">
 
-      <div class="user-info">
-
-        user info
-      </div>
-
-
-
-
+      <ProfileInfo />
 
       <div class="friends">
 
-        <div>all firends bar</div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 bg-am-200 py-3 px-4 rounded-xl my-5">
+          <div>All Friends</div>
+          <div class="self-end">input</div>
+        </div>
 
-        <div class="grid grid-cols-4 gap-4">
+
+
+        <div class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 my-5">
           <ProfileUser v-for="user in users" :key="user.id" :user="user" />
         </div>
 
       </div>
-
-
-
-
-
     </div>
   </div>
 </template>
