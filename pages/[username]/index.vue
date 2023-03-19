79367e45ef994d52f23f58d4a0edb4cbcfe68eea
refaat1297 <a.refaat1297@gmail.com>
2023-03-19 08:45:47 +0200

    Feat 🚀: add profile search friends section

--- pages/[username]/index.vue
+++ pages/[username]/index.vue
@@ -4,67 +4,67 @@ const users = [
     id: 'dd',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd1',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd2',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd3',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd4',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd5',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd6',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd7',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd8',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd9',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   },
   {
     id: 'dd10',
     name: "Ahmed Refaat",
     username: 'refaat1297',
-    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/milly.jpg'
+    avatar: 'https://friendkit.cssninja.io/assets/img/avatars/bobby.jpg'
   }
 ]
 </script>
@@ -77,9 +77,16 @@ const users = [
 
       <div class="friends">
 
-        <div class="grid grid-cols-1 sm:grid-cols-2 bg-am-200 py-3 px-4 rounded-xl my-5">
-          <div>All Friends</div>
-          <div class="self-end">input</div>
+        <div class="sm:flex sm:items-center sm:items-center sm:justify-between bg-am-200 py-2 px-3 rounded-xl my-5">
+          <div class="text-sm mb-2 sm:mb-0">All Friends</div>
+
+          <div class="flex flex-row-reverse gap-2 w-full sm:w-48 bg-am-100 py-1 px-3 rounded-full">
+            <input type="text"
+                   class="block w-full bg-inherit outline-none border-none
+                          placeholder:text-sm placeholder:text-slate-500 text-sm peer"
+                   placeholder="Search Friends..." />
+            <IconsSearch class="w-[20px] self-center fill-slate-300/70 peer-focus:fill-am-blue-primary transition" />
+          </div>
         </div>
 
 
@@ -92,7 +99,3 @@ const users = [
     </div>
   </div>
 </template>
-
-<style lang="scss">
-
-</style>
